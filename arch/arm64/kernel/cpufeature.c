0ba3a4314ef74a3f2835448dd3cd1a1c658a27fa
log1cs <tuan73176@gmail.com>
2023-02-08 23:06:21 +0000

    arch/arm64: import OEM changes
    Signed-off-by: log1cs <tuan73176@gmail.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -901,6 +901,7 @@ static bool unmap_kernel_at_el0(const struct arm64_cpu_capabilities *entry,
 		MIDR_ALL_VERSIONS(MIDR_CORTEX_A72),
 		MIDR_ALL_VERSIONS(MIDR_CORTEX_A73),
 		MIDR_ALL_VERSIONS(MIDR_KRYO5S),
+		MIDR_ALL_VERSIONS(MIDR_KRYO2XX_SILVER),
 		MIDR_ALL_VERSIONS(MIDR_HISI_TSV110),
 		{ /* sentinel */ }
 	};
