01cf9f8020899e5bef07b4699c68fb8f9c82b9c6
log1cs <tuan73176@gmail.com>
2023-02-08 23:06:58 +0000

    drivers/scsi: import OEM changes
    Signed-off-by: log1cs <tuan73176@gmail.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -44,6 +44,7 @@
 #include <linux/interrupt.h>
 #include <linux/init.h>
 #include <linux/blkdev.h>
+#include <linux/backing-dev.h>
 #include <linux/blkpg.h>
 #include <linux/delay.h>
 #include <linux/mutex.h>
@@ -56,6 +57,8 @@
 #include <linux/t10-pi.h>
 #include <linux/uaccess.h>
 #include <asm/unaligned.h>
+
+#include "../../block/blk.h"
 
 #include <scsi/scsi.h>
 #include <scsi/scsi_cmnd.h>
@@ -2732,6 +2735,10 @@ sd_read_cache_type(struct scsi_disk *sdkp, unsigned char *buffer)
 		if (sdkp->WCE && sdkp->write_prot)
 			sdkp->WCE = 0;
 
+		/* No cache flush allowed for UFS well-known LU */
+		if (sdkp->WCE && (sdp->bootlunID == 1 || sdp->bootlunID == 2))
+			sdkp->WCE = 0;
+
 		return;
 	}
 
@@ -3127,6 +3134,9 @@ static int sd_revalidate_disk(struct gendisk *disk)
 				      (sector_t)BLK_DEF_MAX_SECTORS);
 	}
 
+	/* IOPP-max_sectors-v1.0.4.14 */
+	rw_max = max(rw_max, (unsigned int)BLK_DEF_MAX_SECTORS);
+
 	/* Do not exceed controller limit */
 	rw_max = min(rw_max, queue_max_hw_sectors(q));
 
@@ -3215,6 +3225,42 @@ static int sd_format_disk_name(char *prefix, int index, char *buf, int buflen)
 	return 0;
 }
 
+#if IS_ENABLED(CONFIG_UFS_DATA_LOG)
+static void sd_enable_ufs_data_log(struct scsi_disk *sdkp, struct gendisk *gd)
+{
+	struct scsi_device *sdp = sdkp->device;
+	struct Scsi_Host *host = sdp->host;
+	struct scsi_host_template *sht = host->hostt;
+	struct disk_part_iter piter;
+	struct hd_struct *part;
+
+	if (strncmp(sht->name, "ufshcd", 6) ||
+			strncmp(gd->disk_name, "sda", 3) ||
+			!gd->part_tbl)
+		return;
+
+	host->ufs_system_start = 0;
+	host->ufs_system_end = 0;
+	host->ufs_sys_log_en = false;
+
+	disk_part_iter_init(&piter, gd, 0);
+	while ((part = disk_part_iter_next(&piter))) {
+		if (!strncasecmp(part->info->volname, "super", 5)) {
+			host->ufs_system_start = part->start_sect;
+			host->ufs_system_end = (part->start_sect + part->nr_sects);
+			host->ufs_sys_log_en = true;
+			sd_printk(KERN_NOTICE, sdkp, "UFS data logging enabled\n");
+			sd_printk(KERN_NOTICE, sdkp, "UFS %s partition, from : %ld, to %ld\n",
+					part->info->volname,
+					(unsigned long)host->ufs_system_start,
+					(unsigned long)host->ufs_system_end);
+			break;
+		}
+	}
+	disk_part_iter_exit(&piter);
+}
+#endif
+
 /*
  * The asynchronous part of sd_probe
  */
@@ -3265,7 +3311,9 @@ static void sd_probe_async(void *data, async_cookie_t cookie)
 	device_add_disk(dev, gd);
 	if (sdkp->capacity)
 		sd_dif_config_host(sdkp);
-
+#if IS_ENABLED(CONFIG_UFS_DATA_LOG)
+	sd_enable_ufs_data_log(sdkp, gd);
+#endif
 	sd_revalidate_disk(gd);
 
 	if (sdkp->security) {
@@ -3299,6 +3347,7 @@ static void sd_probe_async(void *data, async_cookie_t cookie)
 static int sd_probe(struct device *dev)
 {
 	struct scsi_device *sdp = to_scsi_device(dev);
+	struct scsi_host_template *sht = sdp->host->hostt;
 	struct scsi_disk *sdkp;
 	struct gendisk *gd;
 	int index;
@@ -3355,6 +3404,31 @@ static int sd_probe(struct device *dev)
 					     SD_MOD_TIMEOUT);
 	}
 
+	if (strncmp(sht->name, "ufshcd", 6)) {
+		struct request_queue *q = sdp->request_queue;
+
+		/* decrease max # of requests to 32. The goal of this tuning is
+		 * reducing the time for draining elevator when elevator_switch
+		 * function is called. It is effective for slow USB memory.
+		 */
+		q->nr_requests = BLKDEV_MAX_RQ / 8;
+		if (q->nr_requests < 32)
+			q->nr_requests = 32;
+#ifdef CONFIG_LARGE_DIRTY_BUFFER
+		/* apply more throttle on non-ufs scsi device */
+		q->backing_dev_info->capabilities |= BDI_CAP_STRICTLIMIT;
+		bdi_set_min_ratio(q->backing_dev_info, 30);
+		bdi_set_max_ratio(q->backing_dev_info, 60);
+#endif
+		pr_info("Parameters for SCSI-dev(%s): min/max_ratio: %u/%u "
+			"strictlimit: on nr_requests: %lu read_ahead_kb: %lu\n",
+			gd->disk_name,
+			q->backing_dev_info->min_ratio,
+			q->backing_dev_info->max_ratio,
+			q->nr_requests,
+			q->backing_dev_info->ra_pages * 4);
+	}
+
 	device_initialize(&sdkp->dev);
 	sdkp->dev.parent = dev;
 	sdkp->dev.class = &sd_disk_class;
@@ -3398,6 +3472,17 @@ static int sd_remove(struct device *dev)
 {
 	struct scsi_disk *sdkp;
 	dev_t devt;
+
+#ifdef CONFIG_LARGE_DIRTY_BUFFER
+	struct scsi_device *sdp;
+
+	/* restore bdi min/max ratio before device removal */
+	sdp = to_scsi_device(dev);
+	if (sdp && sdp->request_queue) {
+		bdi_set_min_ratio(sdp->request_queue->backing_dev_info, 0);
+		bdi_set_max_ratio(sdp->request_queue->backing_dev_info, 100);
+	}
+#endif
 
 	sdkp = dev_get_drvdata(dev);
 	devt = disk_devt(sdkp->disk);
@@ -3502,6 +3587,10 @@ static int sd_start_stop_device(struct scsi_disk *sdkp, int start)
 static void sd_shutdown(struct device *dev)
 {
 	struct scsi_disk *sdkp = dev_get_drvdata(dev);
+	struct scsi_device *sdp = to_scsi_device(dev);
+	struct scsi_host_template *sht = sdp->host->hostt;
+	struct request_queue *q = sdp->request_queue;
+	unsigned long flags;
 
 	if (!sdkp)
 		return;         /* this can happen */
@@ -3517,6 +3606,14 @@ static void sd_shutdown(struct device *dev)
 	if (system_state != SYSTEM_RESTART && sdkp->device->manage_start_stop) {
 		sd_printk(KERN_NOTICE, sdkp, "Stopping disk\n");
 		sd_start_stop_device(sdkp, 0);
+	}
+
+	if (!strncmp(sht->name, "ufshcd", 6)) {
+		spin_lock_irqsave(q->queue_lock, flags);
+		queue_flag_set(QUEUE_FLAG_DYING, q);
+		__blk_drain_queue(q, true);
+		queue_flag_set(QUEUE_FLAG_DEAD, q);
+		spin_unlock_irqrestore(q->queue_lock, flags);
 	}
 }
 
